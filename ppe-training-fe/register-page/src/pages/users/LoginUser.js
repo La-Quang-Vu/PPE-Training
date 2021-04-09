642be7c7115f9b53899abc13c44e2a734b450cba
La-Quang-Vu <laquangvu2007@gmail.com>
2021-04-09 10:02:10 +0700

    + Add cookie to save token

--- ppe-training-fe/register-page/src/pages/users/LoginUser.js
+++ ppe-training-fe/register-page/src/pages/users/LoginUser.js
@@ -2,8 +2,11 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import Alert from '../../components/Alert';
+import Cookies from 'universal-cookie';
 
 function LoginUser() {
+  const cookies = new Cookies();
+
     const [message,setMessage] = useState(null) 
 
     const loginUser = async (e) => {
@@ -18,6 +21,7 @@ function LoginUser() {
                 // setMessage('Login successful');
 
                 Alert({t: `success`, c: [`login success`]});
+                cookies.set('ppe-training-fe-token', response.data.data.token , { path: '/', expires: new Date(Date.now()+25920000000)});
             } else {
                 // setMessage(msg);
                 Alert({t: `error`, c: [msg]});
@@ -85,11 +89,11 @@ function LoginUser() {
           <span className="absolute absolute-x absolute-y bg-white px-3 mt-px text-sm text-gray-400">or login with</span>
         </div>
         <div className="flex space-x-3 mt-5">
-          <button className="flex item-center px-4 py-2 rounded font-bold w-full text-red-600 border border-red-600">
+          <button type="button" className="flex item-center px-4 py-2 rounded font-bold w-full text-red-600 border border-red-600">
             <img className="w-5 mr-4" src=".\assets\images\google-icon.png" />
             Google
           </button>
-          <button className="flex item-center px-4 py-2 rounded font-bold w-full text-blue-600 border border-blue-600">
+          <button type="button" className="flex item-center px-4 py-2 rounded font-bold w-full text-blue-600 border border-blue-600">
             <img className="w-7 mr-2" src=".\assets\images\facebook-icon1.png" />
             Facebook
           </button>
--- ppe-training-fe/register-page/src/pages/users/RegisterUser.js
+++ ppe-training-fe/register-page/src/pages/users/RegisterUser.js
@@ -2,10 +2,11 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
 import Alert from '../../components/Alert';
-
+import Cookies from 'universal-cookie';
 
 function RegisterUser() {
   const [message,setMessage] = useState(null) 
+  const cookies = new Cookies();
 
   const createUser = async (e) => {
     e.preventDefault();
@@ -16,6 +17,7 @@ function RegisterUser() {
       .then(function (response) {
         // setMessage('Register user success')
         Alert({t: 'success', c: ['Register user success']})
+        cookies.set('ppe-training-fe-token', response.data.data.token , { path: '/', expires: new Date(Date.now()+25920000000)});
         console.log(response);
       })
       .catch(function (error) {
@@ -80,11 +82,11 @@ function RegisterUser() {
           <span className="absolute absolute-x absolute-y bg-white px-3 mt-px text-sm text-gray-400">or</span>
         </div>
         <div className="flex space-x-3 mt-5">
-          <button className="flex item-center px-4 py-2 rounded font-bold w-full text-red-600 border border-red-600">
+          <button type="button" className="flex item-center px-4 py-2 rounded font-bold w-full text-red-600 border border-red-600">
             <img className="w-5 mr-4" src=".\assets\images\google-icon.png" />
             Google
           </button>
-          <button className="flex item-center px-4 py-2 rounded font-bold w-full text-blue-600 border border-blue-600">
+          <button type="button" className="flex item-center px-4 py-2 rounded font-bold w-full text-blue-600 border border-blue-600">
             <img className="w-7 mr-2" src=".\assets\images\facebook-icon1.png" />
             Facebook
           </button>
